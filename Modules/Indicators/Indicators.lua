dc2928d114c1060637a6f84ef550c3344e308e1f
David W Zhang <david.chang.ok@gmail.com>
2026-06-15 06:41:40 +0800

    fix: glow alpha 0.4→0.5, 同步Indicator预览版本

--- Modules/Indicators/Indicators.lua
+++ Modules/Indicators/Indicators.lua
@@ -422,7 +422,7 @@ local function InitIndicator(indicatorName)
             end
 
             if found and self.glow then
-                self.glow:SetBackdropColor(r, g, b, 0.3)
+                self.glow:SetBackdropColor(r, g, b, 0.5)
                 self.glow:Show()
             elseif self.glow then
                 self.glow:SetBackdropColor(0, 0, 0, 0)
