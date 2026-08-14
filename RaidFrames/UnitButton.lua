cc36155d3fcaa4ae4e24f4ebad1890ab4e90c997
David W Zhang <david.chang.ok@gmail.com>
2026-08-15 07:59:01 +0800

    feat: v1.1.0 AuraContainer 官方通道 - 12.1 光环容器集成, 战斗中显示被 secret 化的增益(图标+倒计时由游戏引擎渲染), 施放追踪暂时停用

--- RaidFrames/UnitButton.lua
+++ RaidFrames/UnitButton.lua
@@ -3242,6 +3242,9 @@ local function UnitButton_OnAttributeChanged(self, name, value)
         if type(value) == "string" then
             self.states.unit = value
             self.states.displayedUnit = value
+            if self.widgets.aurac then
+                self.widgets.aurac:SetUnit(value)
+            end
             if string.find(value, "^raid%d+$") then Cell.unitButtons.raid.units[value] = self end
 
             -- range
@@ -4475,6 +4478,7 @@ function CellUnitButton_OnLoad(button)
     I.CreateDispels(button)
     I.CreateRaidDebuffs(button)
     I.CreatePrivateAuras(button)
+    I.CreateAuraContainer(button)
     I.CreateTargetedSpells(button)
     I.CreateTargetCounter(button)
     I.CreateCrowdControls(button)
